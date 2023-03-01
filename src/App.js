ced434cc435f5221b7ffb2975dc96ed5ae6f4ac0
Andrii <nikulin.and90@gmail.com>
2023-03-01 22:10:40 +0200

    add slider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import logo from './logo.svg';
 import './App.scss';
+import "slick-carousel/slick/slick.scss"; 
+import "slick-carousel/slick/slick-theme.scss";
 import Header from './Companents/Header/Header';
 import React from 'react';
 
--- src/Companents/Header/Header.js
+++ src/Companents/Header/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import $ from 'jquery';
+// import Slider from 'react-slick';
 import icons from '../../images/sprites/spriteMy.svg';
 
 
@@ -8,9 +10,48 @@ const Header = () => {
         <div className='header__slider'>
             <div className='eader__slider-item slider-item'>
                 <div className='slider-item__info'>
-                    <div className='slider-item__info-suptitle'>Surf</div>
+                    <div className='slider-item__info-suptitle suptitle-left'>Surf</div>
+                    <div className='slider-item__info-title'>North Shore</div>
+                    <div className='slider-item__info-supttext suptitle-left'>Condition</div>
+                    <div className='slider-item__info-text'>Condition</div>
+                    <a className='slider-item__info-link' href="#">
+                        <svg>
+                            <use href={`${icons}#arrowsRight`}></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div className='eader__slider-item slider-item'>
+                <div className='slider-item__info'>
+                    <div className='slider-item__info-suptitle suptitle-left'>Surf</div>
+                    <div className='slider-item__info-title'>South Shore</div>
+                    <div className='slider-item__info-supttext suptitle-left'>Condition</div>
+                    <div className='slider-item__info-text'>Condition</div>
+                    <a className='slider-item__info-link' href="#">
+                        <svg>
+                            <use href={`${icons}#arrowsRight`}></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div className='eader__slider-item slider-item'>
+                <div className='slider-item__info'>
+                    <div className='slider-item__info-suptitle suptitle-left'>Surf</div>
                     <div className='slider-item__info-title'>West Shore</div>
-                    <div className='slider-item__info-supttext'>Condition</div>
+                    <div className='slider-item__info-supttext suptitle-left'>Condition</div>
+                    <div className='slider-item__info-text'>Condition</div>
+                    <a className='slider-item__info-link' href="#">
+                        <svg>
+                            <use href={`${icons}#arrowsRight`}></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div className='eader__slider-item slider-item'>
+                <div className='slider-item__info'>
+                    <div className='slider-item__info-suptitle suptitle-left'>Surf</div>
+                    <div className='slider-item__info-title'>East Shore</div>
+                    <div className='slider-item__info-supttext suptitle-left'>Condition</div>
                     <div className='slider-item__info-text'>Condition</div>
                     <a className='slider-item__info-link' href="#">
                         <svg>
